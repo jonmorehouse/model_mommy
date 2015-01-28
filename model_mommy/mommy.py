525617ccd07fa5cfa3b469ce57184943f11409d8
Jon Morehouse <morehousej09@gmail.com>
2015-01-27 18:26:03 -0800

    Add in attributes functionality

--- model_mommy/mommy.py
+++ model_mommy/mommy.py
@@ -292,9 +292,10 @@ class Mommy(object):
         _extra_%s = build extra parameters, passing in either the current hash or the current object
         '''
         fill_in_optional = attrs.pop('_fill_optional', False)
-        extra_attrs = dict((k, attrs.pop(k)) for k in attrs.keys() if '_extra_' in k)
         ignore = attrs.pop('_ignore', [])
         ignore_related = bool(attrs.pop('_ignore_related', False))
+        [attrs.pop(k) for k in ignore]
+        extra_attrs = dict((k, attrs.pop(k)) for k in attrs.keys() if '_extra_' in k)
 
         is_rel_field = lambda x: '__' in x
         iterator_attrs = dict((k, v) for k, v in attrs.items() if is_iterator(v))
